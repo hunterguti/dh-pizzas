f12612bf6e544abe4cbae660a28e36675169967b
hunterguti <38108699+hunterguti@users.noreply.github.com>
2019-11-12 17:26:19 -0300

    12-11

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = requiere('fs');
+
+
+const faqs = require('./data/faqs.json') 
+const movies = require('./data/faqs.json') 
+const theaters = require('./data/faqs.json') 
+//servidor
+
+http.createServer((req, res) => {
+
+   res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'}) ;
+
+   //route system 
+
+
+
+
+}
